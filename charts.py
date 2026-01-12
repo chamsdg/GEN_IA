8fb1e7f61de7f28dba01a1dc56e4065b0efd3602
Chamsedine AIDARA <50946856+chamsdg@users.noreply.github.com>
2026-01-12 09:15:23 +0000

    Update charts.py
    update charts

--- charts.py
+++ charts.py
@@ -29,6 +29,62 @@ def build_line_chart(
     return fig
 
 
+
+
+def build_multi_line_chart(
+    df,
+    x_col,
+    y_col,
+    color_col,
+    title,
+    y_label
+):
+    fig = px.line(
+        df,
+        x=x_col,
+        y=y_col,
+        color=color_col,
+        markers=True,
+        title=title,
+        text=y_col
+    )
+
+    # Format des valeurs affichées sur les points
+    fig.update_traces(
+        texttemplate="%{y:.2s} €",  # ex: 8.9M €
+        textposition="top center"
+    )
+
+    # Mise en forme du graphique
+    fig.update_layout(
+        yaxis=dict(
+            title=y_label,
+            tickformat="~s"  # ex: 1k, 1M, 10M
+        ),
+        xaxis_title="Mois",
+        hovermode="x unified",
+        legend_title_text="Client",
+        template="plotly_white"
+    )
+
+    return fig
+
+
+
+def build_evolution_title(clients: list) -> str:
+    if not clients:
+        return "📈 Évolution mensuelle des ventes (global)"
+
+    if len(clients) == 1:
+        return f"📈 Évolution mensuelle des ventes – {clients[0]}"
+
+    # Cas comparaison
+    clients_str = " vs ".join(clients)
+    return f"📈 Évolution mensuelle des ventes – {clients_str}"
+
+
+
+"""
 def build_multi_line_chart(
     df,
     x_col,
@@ -55,3 +111,4 @@ def build_multi_line_chart(
     )
 
     return fig
+"""
